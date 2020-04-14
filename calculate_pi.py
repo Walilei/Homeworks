fc9659811319e5f658d78d6c67a80b73cd91e42b
Walilei <walilei@gmail.com>
2020-04-15 01:06:43 +0800

    Update calculate_pi.py

--- calculate_pi.py
+++ calculate_pi.py
@@ -4,19 +4,11 @@ n = int(input('Enter dots num: '))
 red = 0
 blue = n
 
-
-class Dot:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-
-
 for _ in range(n):
-    _ = Dot()
-    _.x = random.random()
-    _.y = random.random()
+    x = random.random()
+    y = random.random()
 
-    if _.x ** 2 + _.y ** 2 <= 1:
+    if x ** 2 + y ** 2 <= 1:
         red += 1
 
 print(4 * red / blue)

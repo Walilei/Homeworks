dee7cb26003917b20286ff15a8f10b633792e9c8
Walilei <walilei@gmail.com>
2020-04-18 02:54:50 +0800

    Update time.py

--- time.py
+++ time.py
@@ -1,26 +1,26 @@
-class Time():
-    def __init__(self, hours=0, mins=0):
+class Time:
+    def __init__(self, hours=0, minutes=0):
         self.hours = hours
-        self.mins = mins
+        self.minutes = minutes
 
-    def addTime(self, t1, t2):
+    def add_time(self, t1, t2):
         self.hours = t1.hours + t2.hours
-        self.mins = t1.mins + t2.mins
-        while self.mins >= 60:
+        self.minutes = t1.minutes + t2.minutes
+        while self.minutes >= 60:
             self.hours += 1
-            self.mins -= 60
+            self.minutes -= 60
         return self
 
     def display_time(self):
-        print(f"Time is {self.hours} hours and {self.mins} minutes.")
+        print(f"Time is {self.hours} hours and {self.minutes} minutes.")
 
     def display_minute(self):
-        minutes = self.hours * 60 + self.mins
+        minutes = self.hours * 60 + self.minutes
         print(f"total {minutes} minutes.")
 
 
 a = Time(2, 59)
 b = Time(1, 20)
-c = Time().addTime(a, b)
+c = Time().add_time(a, b)
 c.display_time()
 c.display_minute()

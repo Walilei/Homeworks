acb0fbc1a3897d5d0f913b58da2d54d09c02e484
Walilei <walilei@gmail.com>
2020-04-26 19:55:38 +0800

    Update web crawler - ptt.py

--- web crawler - ptt.py
+++ web crawler - ptt.py
@@ -3,14 +3,13 @@ from bs4 import BeautifulSoup
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'}
-url = 'https://www.ptt.cc/bbs/movie/M.1587888439.A.E83.html'
+url = 'https://www.ptt.cc/bbs/movie/M.1587832086.A.174.html'
 req = requests.get(url, headers=headers)
 soup = BeautifulSoup(req.text, 'html.parser')
 
-
-content = soup.find('div', {'id':'main-content'}).text.split('※')[0]
+content = soup.find('div', {'id': 'main-content'}).text.split('※')[0]
 info = []
-for i in soup.find_all('span', {'class':'article-meta-value'}):
+for i in soup.find_all('span', {'class': 'article-meta-value'}):
     info.append(i.text)
 author = info[0]
 title = info[2]
@@ -18,13 +17,13 @@ time = info[3]
 
 like = 0
 dislike = 0
-for i in soup.find_all('div', {'class':'push'}):
+for i in soup.find_all('div', {'class': 'push'}):
     if i.text[0] == '推':
         like += 1
     elif i.text[0] == '噓':
         dislike += 1
 score = like - dislike
-        
+
 print(content.strip('\n').strip('--'))
 print('--我是分隔線--')
 print(f"推： {like}")

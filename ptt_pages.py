08c570e9f2fde5111a0db6447d03f742ddbd866b
Walilei <walilei@gmail.com>
2020-04-30 15:38:07 +0800

    Update and rename web crawler - ptt (pages).py to ptt_pages.py

--- ptt_pages.py
+++ ptt_pages.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from ptt_web_crawler import ptt_to_txt
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) '
                           'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
@@ -13,6 +14,8 @@ soup = BeautifulSoup(req.text, 'html.parser')
 while page > 8930:
     for tag in soup.find_all('div', {'class': 'title'}):
         web = 'https://www.ptt.cc/' + tag.find('a')['href']
-        print(web)
+        res = requests.get(web, headers=headers, cookies=cookies)
+        article_soup = BeautifulSoup(res.text, 'html.parser')
+        ptt_to_txt(article_soup)
     page -= 1
-
+   

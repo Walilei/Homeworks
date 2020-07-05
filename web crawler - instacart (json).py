8e8a17c3e4d576261f88034c40691a6e8b40ae63
Walilei <walilei@gmail.com>
2020-07-06 04:42:05 +0800

    Update web crawler - instacart (json).py

--- web crawler - instacart (json).py
+++ web crawler - instacart (json).py
@@ -5,33 +5,38 @@ import csv
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/83.0.4103.116 Safari/537.36'
 product_info = {}
-spec_list = ["Contains", "Form", "State of Readiness", "Store", "Package Quantity", "Package type", "Net weight"]  # 要取得的specifications項目
+# 要取得的specifications項目
+spec_list = ["Contains", "Form", "State of Readiness", "Store", "Package Quantity", "Package type", "Net weight"]
 
-with open('keywords.csv', newline='', encoding='utf-8', errors='ignore') as csvfile:
+
+with open('keywords.csv', newline='', encoding='utf-8', errors='ignore') as csv_file:
     count = 0
     index = 1
-    products = csv.reader(csvfile)
+    products = csv.reader(csv_file)
     for item in products:
         product_query = item[0] + ', ' + item[1]
         product_query = quote(product_query)
-        json_url = 'https://redsky.target.com/v2/plp/search/?' \
-                   'channel=web&count=6&default_purchasability_filter=true' \
-                   '&facet_recovery=false&fulfillment_test_mode=grocery_opu_team_member_test' \
-                   f'&isDLP=false&keyword={product_query}&offset=0&pageId=%2Fs%2F{product_query}&pricing_store_id=2776' \
-                   '&store_ids=2776%2C3277%2C3249%2C3312%2C3284&visitorId=01730534EA6202019A76E876D860158E' \
-                   '&include_sponsored_search_v2=false&ppatok=AOxT33a&platform=desktop' \
-                   f'&useragent={user_agent}' \
-                   '&excludes=available_to_promise_qualitative%2Cavailable_to_promise_location_qualitative' \
-                   '&key=eb2551e4accc14f38cc42d32fbc2b2ea'
+        json_url = "https://redsky.target.com/v2/plp/search/?" \
+                   "channel=web&count=6&default_purchasability_filter=true" \
+                   "&facet_recovery=false&fulfillment_test_mode=grocery_opu_team_member_test" \
+                   f"&isDLP=false&keyword={product_query}&offset=0&pageId=%2Fs%2F{product_query}" \
+                   f"&pricing_store_id=2776" \
+                   "&store_ids=2776%2C3277%2C3249%2C3312%2C3284&visitorId=01730534EA6202019A76E876D860158E" \
+                   "&include_sponsored_search_v2=false&ppatok=AOxT33a&platform=desktop" \
+                   f"&useragent={user_agent}" \
+                   "&excludes=available_to_promise_qualitative%2Cavailable_to_promise_location_qualitative" \
+                   "&key=eb2551e4accc14f38cc42d32fbc2b2ea"
         try:
             req = requests.get(json_url)        # 搜尋不到物品時會報錯
             json_string = json.loads(req.text)  # python的字典格式
-        except:
+            p_nums = len(json_string['search_response']['items']['Item'])
+        except KeyError:
             print(f'No result for {item[0]}.')
+            continue
 
-        p_nums = len(json_string['search_response']['items']['Item'])
         for i in range(p_nums):
             product_info['query'] = item[0]
             product = json_string['search_response']['items']['Item'][i]
@@ -51,8 +56,8 @@ with open('keywords.csv', newline='', encoding='utf-8', errors='ignore') as csvf
                 product_info['average_rating'] = (product["average_rating"], product["total_reviews"])  # 評分分數和人數
             if 'wellness_merchandise_attributes' in product.keys():
                 product_info['At a glance'] = []
-                for i in product['wellness_merchandise_attributes']:
-                    product_info['At a glance'].append(i['value_name'])  # 商品特別標籤
+                for _ in product['wellness_merchandise_attributes']:
+                    product_info['At a glance'].append(_['value_name'])  # 商品特別標籤
             if 'soft_bullets' in product.keys():
                 if 'bullets' in product['soft_bullets'].keys():
                     product_info['highlights'] = product['soft_bullets']['bullets']  # 商品特色
